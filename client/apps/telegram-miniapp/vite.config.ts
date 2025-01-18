f4521471e2eb99c3d5c5ebc235c0d52b960470f8
0x1337 <za6ah9t@icloud.com>
2025-01-18 19:53:57 +0700

    feat: telegram miniapp certs

--- client/apps/telegram-miniapp/vite.config.ts
+++ client/apps/telegram-miniapp/vite.config.ts
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+// @ts-nocheck
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
+import mkcert from "vite-plugin-mkcert";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-})
+export default defineConfig(() => ({
+  plugins: [react(), mkcert()],
+  server: {
+    https: true,
+    host: true,
+  },
+}));
